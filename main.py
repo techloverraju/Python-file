72ff8e467fe626d8bd62cc569358869ed65259cd
techloverraju <104580398+techloverraju@users.noreply.github.com>
2022-04-28 20:54:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+minimum = int(input(" Please Enter the Minimum Value: "))
+maximum = int(input(" Please Enter the Maximum Value: "))
+total = 0
+
+for Number in range (minimum, maximum + 1):
+    count = 0
+    for i in range(2, (Number//2 + 1)):
+        if(Number % i == 0):
+            count = count + 1
+            break
+
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '  ')
+        total = total + Number
+
+print("\n\nSum from %d to %d = %d" %(minimum, maximum, total))
